30b2da62bf779c25e640ed0ea38b2e7308e7f64c
Amanda Pereira <estudosjpereira.amanda@gmail.com>
2025-07-03 14:13:05 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SobreComponent } from './components/sobre/sobre.component';
+import { CompetenciasComponent } from './components/competencias/competencias.component';
+import { LinksComponent } from './components/links/links.component';
+import { EstudosComponent } from './components/estudos/estudos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SobreComponent,
+    CompetenciasComponent,
+    LinksComponent,
+    EstudosComponent
   ],
   imports: [
     BrowserModule,
